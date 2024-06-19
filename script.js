363cc819f4fca70cfec8c9f1e99da43e811b0a9c
shucksow <102425362+shucksow@users.noreply.github.com>
2024-06-19 18:07:22 -0400

    Finish Calculator Object

--- script.js
+++ script.js
@@ -1,19 +1,76 @@
-function add(a, b){
+const inputDisplay = document.getElementById('input');
+const outputDisplay = document.getElementById('output');
+
+function handleButtonDisplay(event) {
+    const button = event.target;
+
+    if(button.classList.contains('button')) {
+        const buttonText = button.textContent;
+
+        switch (button.id) {
+            case 'clear':
+                calculator.clear();
+                break;
+            case 'sign':
+                break;
+            case 'percent':
+                break;
+            case 'add':
+                calculator.operator = '+'
+                break;
+            case 'subtract':
+                calculator.operator = '-'
+                break;
+            case 'multiply':
+                calculator.operator = '*'
+                break;
+            case 'divide':
+                calculator.operator = '/'
+                break;
+            default:
+                break;
+    }
+
+        }
 
 }
 
-function subtract(a, b){
 
-}
 
-function multiply(a, b){
+const calculator = {
+    operand1: 0,
+    operand2: 0,
+    operator: '',
+    result: 0,
 
-}
+    setOperand1: function(op1) {
+       return this.operand1 = op1; 
+    },
 
-function divide(a, b){
+    setOperand2: function(op2) {
+        return this.operand2 = op2;
+    },
 
-}
+    getSum: function() {
+        return this.result = this.operand1 + this.operand2;
+    },
 
-function calculatePercentage(){
-    
+    getDifference: function() {
+        this.result = this.operand1 - this.operand2;
+    },
+
+    getProduct: function() {
+        this.result = this.operand1 * this.operand2;
+    },
+
+    getQuotient: function() {
+        this.result = this.operand1 / this.operand2;
+    },
+
+    clear: function() {
+        this.operand1 = 0;
+        this.operand2 = 0;
+        this.operator = '';
+        this.result = 0;
+    }
 }
